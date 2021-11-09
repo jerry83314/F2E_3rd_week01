f1ec47471fd9da5f3022bdb124e3a81ab311f1dd
Jerry Huang <jerry83314@gmail.com>
2021-11-09 16:34:33 +0800

    add: npm vue-axios

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import store from './store'
 // Element UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// Vue axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
