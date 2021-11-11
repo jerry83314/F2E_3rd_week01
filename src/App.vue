41b592321109565025b38466a47b03213c270263
Jerry Huang <jerry83314@gmail.com>
2021-11-11 19:06:43 +0800

    add: 試著串接 API(success)

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,46 @@
   </div>
 </template>
 
+<script>
+/* eslint-disable */
+import jsSHA from 'jssha'
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {
+    getData () {
+      this.axios.get('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=30&$format=JSON')
+        .then((res) => {
+          console.log('res', res)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    getAuthorizationHeader () {
+      //  填入自己 ID、KEY 開始
+      const AppID = '7c9053f3cf1045e38cedce5138cd65fb'
+      const AppKey = 'GlSjHMEjLwXCWyoEdX_nscVoxhI'
+      //  填入自己 ID、KEY 結束
+      const GMTString = new Date().toGMTString()
+      const ShaObj = new jsSHA('SHA-1', 'TEXT')
+      ShaObj.setHMACKey(AppKey, 'TEXT')
+      ShaObj.update('x-date: ' + GMTString)
+      const HMAC = ShaObj.getHMAC('B64')
+      const Authorization =
+        'hmac username="' +
+        AppID +
+        '", algorithm="hmac-sha1", headers="x-date", signature="' +
+        HMAC +
+        '"'
+      return { Authorization: Authorization, 'X-Date': GMTString }
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
