bf680317525e38ffb37c78f4030e7ed965fcc44d
Jerry Huang <jerry83314@gmail.com>
2021-11-11 22:09:34 +0800

    add: 完成 footer 切版

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="footer-wrap text-center">
+    <p class="mb-0">Taiwan Tourguide  © Code: Jyue Huang /  Design: KT</p>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   },
   data () {
     return {}
